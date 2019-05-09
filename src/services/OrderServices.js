01d41de70b48351d7bbb9954e802df514a6e07fa
Hoang Trung <lhtrung307@gmail.com>
2019-05-09 17:55:26 +0700

    add createOrder to create new order for customer, isCustomerOrderEmpty to check customer have any order, getCustomerOrderItems to get all items of order

--- src/services/OrderServices.js
+++ src/services/OrderServices.js
@@ -1,18 +1,41 @@
 import React, { Component } from "react";
 /*get date create bill in link https://api-mobile-shopping.herokuapp.com/api/orders/*/
 import { apiLinks } from "./ApiLink";
-import { getProduct } from "./ProductServices";
+
 import Axios from "axios";
+
+const ORDERING =
+  "https://api-mobile-shopping.herokuapp.com/api/ref-order-status-list/0/";
 
 function getBillDate() {
   return Axios.get(apiLinks.orders).then((response) => response.data.results);
 }
 
-function getCustomerOrderItems(customer_id) {
-  let cartProducts = [];
-  Axios.get(apiLinks.orderByCustomer + customer_id)
-    .then((response) => getOrderItemsOfOrder(response.data[0].id))
-    .then((response) => {response.});
+export function getCustomerOrderItems(customer_id) {
+  return Axios.get(apiLinks.orderByCustomer + customer_id).then((response) =>
+    getOrderItemsOfOrder(response.data[0].id)
+  );
+}
+
+export function isCustomerOrderEmpty(customer_id) {
+  let isEmpty = false;
+  Axios.get(apiLinks.orderByCustomer + customer_id).then((response) => {
+    isEmpty = Object.keys(response.data).length === 0;
+  });
+  return isEmpty;
+}
+
+export function createOrder(customer) {
+  let data = {
+    customer_id: customer.url,
+    order_status_code: ORDERING,
+    order_detail: ""
+  };
+  let auth = {
+    username: "admin",
+    password: "admin"
+  };
+  return Axios.post(apiLinks.orders, data, { auth: auth });
 }
 
 function getOrderItemsOfOrder(order_id) {
